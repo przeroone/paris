9f6511ee24845fb34bbec3caac816541a25b1d61
Planet <sun2>
2013-10-29 17:26:53 +0800

    Initial commi

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Paris::Application.config.secret_key_base = 'a36591f22d7c608c2e14adf9daa543ac7854d5e2174365984d2a8035a7186db00929ea70bbc4d7a1d6baee6d2d0acab5f76e963b4d5860aaee542c5f01a9541b'
